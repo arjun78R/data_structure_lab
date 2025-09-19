5193e3e786ed26ad4cd0a7d58ae18346db0773f0
arjun78R <arju63191@gmail.com>
2025-09-19 16:21:49 +0530

    Add files via upload

---
+++ sum.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int main()
+{
+   int ary[50]
+   int n,i,sum=0;
+   printf("enter the number of elements")
+   scanf("%d",&n);
+   if(n<=50)
+   {
+       
+       printf("enter the element of array\n")
+       for(i=0;i<n;i++)
+       {
+       
+            printf("enter the %d element")
+            scanf(%d",&arr[i]);
+       }
+       for(i=0;i<=n;i++)
+       {
+          sum=sum+arr[i];
+        }
+         printf("sum of %d\n",n,sum);
+   }
+   else
+   {
+         printf("the entered size is larger than any array size\n");
+   }
+   return 0;
+}       
+         
